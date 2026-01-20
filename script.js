a57ef1aff0aebd05450fc6a666020fe5103b11a7
mudesasi2000-max <mudesasi2000@gmail.com>
2026-01-20 21:58:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1168,7 +1168,7 @@ function generateBillPreview() {
     billContent.innerHTML = `
         <div class="bill-header-no-logo">
             <div class="bill-clinic-info">
-                <h2>ANASUYA MEDICALS/h2>
+                <h2>ANASUYA MEDICALS</h2>
                 <p>D.No. 26/3/1564, Near GVRR College,
 B.V. Nagar, Nellore - 524 004.</p>
                 <p>Phone: +91 8309303688 | Email: anasuyamedicals242@gmail.com</p>
@@ -1764,3 +1764,4 @@ window.deleteBill = deleteBill;
 window.deleteAllSales = deleteAllSales;
 
 window.viewBillDetails = viewBillDetails;
+
